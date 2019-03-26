fdaeb23eed8a369cb36c189a135c2dc6bd63974d
asas12 <john13john@interia.pl>
2019-03-26 22:31:36 +0100

    Initial commit with JGroups project.

---
+++ jgroups_hashing_table/src/Main.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.setProperty("java.net.preferIPv4Stack","true");
+
+        MyMap myMap = new MyMap();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String in;
+        boolean close = false;
+
+        while(!close){
+            in = br.readLine();
+            String[] tokens = in.split(" ");
+            try {
+                switch (tokens[0]) {
+                    case "get":
+                        System.out.println("Getting: " + tokens[1]);
+                        System.out.println(myMap.get(tokens[1]));
+                        break;
+                    case "contains":
+                        System.out.println("Checking if contains: " + tokens[1]);
+                        System.out.println(myMap.containsKey(tokens[1]));
+                        break;
+                    case "put":
+                        System.out.println("Putting number: " + tokens[2] + " under: " + tokens[1]);
+                        myMap.put(tokens[1], Integer.parseInt(tokens[2]));
+                        break;
+                    case "remove":
+                        System.out.println("Removing: " + tokens[1]);
+                        System.out.println(myMap.remove(tokens[1]));
+                        break;
+                    case "close":
+                        System.out.println("Closing channel.");
+                        close = true;
+                        break;
+                    default:
+                        System.out.println("Unknown command.");
+
+                }
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Too few arguments!");
+            }
+        }
+        myMap.close();
+
+    }
+}
---
+++ jgroups_hashing_table/src/MyMap.java
@@ -0,0 +1,147 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Collections;
+import java.util.HashMap;
+
+import org.jgroups.*;
+import org.jgroups.protocols.*;
+import org.jgroups.protocols.pbcast.*;
+import org.jgroups.stack.ProtocolStack;
+import org.jgroups.util.Util;
+
+public class MyMap implements SimpleStringMap {
+
+    private JChannel channel;
+    private final HashMap<String, Integer> mapInstance;
+
+
+    public MyMap() throws Exception {
+        mapInstance = new HashMap<String, Integer>();
+
+        channel = new JChannel(false);
+
+        ProtocolStack stack=new ProtocolStack();
+        channel.setProtocolStack(stack);
+        stack.addProtocol(new UDP().setValue("mcast_group_addr",InetAddress.getByName("230.100.200.52")))
+                .addProtocol(new PING())
+                .addProtocol(new MERGE3())
+                .addProtocol(new FD_SOCK())
+                .addProtocol(new FD_ALL().setValue("timeout", 12000).setValue("interval", 3000))
+                .addProtocol(new VERIFY_SUSPECT())
+                .addProtocol(new BARRIER())
+                .addProtocol(new NAKACK2())
+                .addProtocol(new UNICAST3())
+                .addProtocol(new STABLE())
+                .addProtocol(new GMS())
+                .addProtocol(new UFC())
+                .addProtocol(new MFC())
+                .addProtocol(new FRAG2())
+                .addProtocol(new STATE_TRANSFER());
+
+        // TODO chyba musi być flush etc
+
+        stack.init();
+
+        setReceiver(channel);
+
+        channel.connect("hashMapCluster");
+
+        channel.getState(null, 0);
+
+        System.out.println("Finished setting up.\nCommands: put <key> <val>; get <key>; contains <key>; remove <key>; close.");
+    }
+
+    public boolean containsKey(String key){
+        return mapInstance.containsKey(key);
+    }
+
+    public Integer get(String key){
+        return mapInstance.get(key);
+    }
+
+    public void put(String key, Integer value) {
+        synchronized (mapInstance) {
+            Integer was = mapInstance.put(key, value);
+            String m = "p " + key + " " + value;
+            Message msg = new Message(null, null, m);
+            try {
+                channel.send(msg);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Was: " + was + ", is: "+ value);
+        }
+    }
+
+    public Integer remove(String key){
+        Integer returnedValue;
+        synchronized (mapInstance){
+            returnedValue = mapInstance.remove(key);
+            String m = "r " + key + " " + returnedValue;
+            Message msg = new Message(null, null, m);
+            try {
+                channel.send(msg);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return returnedValue;
+    }
+
+
+
+    public void close(){
+        //TODO disconnect rather???
+        channel.close();
+    }
+
+    private void setReceiver(Channel channel){
+        channel.setReceiver(new ReceiverAdapter(){
+            @Override
+            public void viewAccepted(View view) {
+                // wyświetla listę instancji w klastrze przy wchodzeniu/ wychodzeniu
+                super.viewAccepted(view);
+                System.out.println("Instances: " + view.toString());
+            }
+            public void receive(Message msg) {
+                // odpala się przy otrzymaniu wiadomości
+                System.out.println("Received msg from "+ msg.getSrc() + ": "+ msg.getObject());
+                synchronized (mapInstance){
+                    String info = new String(msg.getObject().toString());
+                    String[] tokens = info.split(" ");
+                    if(tokens[0].equals("r")){
+                        mapInstance.remove(tokens[1]);
+                        System.out.println("Removing "+ tokens[1]);
+                    }else{
+                        System.out.println(tokens[0]);
+                        System.out.println(tokens[0].equals("r"));
+                        mapInstance.put(tokens[1], Integer.parseInt(tokens[2]));
+                        System.out.println("Updating "+ tokens[1]);
+                        //this.;
+                    }
+                }
+            }
+            public void getState(OutputStream output) throws Exception {
+                synchronized (mapInstance){
+                    Util.objectToStream(mapInstance, new DataOutputStream(output));
+                }
+                System.out.println("Got state");
+
+            }
+
+            public void setState(InputStream input) throws Exception {
+                HashMap<String, Integer> map;
+                map = (HashMap<String, Integer>) Util.objectFromStream(new DataInputStream(input));
+                synchronized (mapInstance){
+                    mapInstance.clear();
+                    mapInstance.putAll(map);
+                }
+                System.out.println("Got state: " + Collections.singletonList(map));
+            }
+        });
+    }
+}
